600620af74a8c8aa9be738cc0eac76e06a7a9009
dasilvac9u <alexandre.da-silva-carmo9@etu.univ-lorraine.fr>
2019-01-15 21:35:17 +0100

    merge

--- src/views/View.php
+++ src/views/View.php
@@ -36,11 +36,7 @@ abstract class View {
 		} else {
 			$this->nav = "<li><a href='{$this->app->urlFor('index')}'>Accueil</a></li><li><a href='{$this->app->urlFor('liste.publique')}'>Voir les listes publiques</a></li>";
 		}
-<<<<<<< HEAD
 
-		
-=======
->>>>>>> 205f4b304186d2a484803ad2fb762b0e3b0c0cb0
 	}
 
 	public abstract function render($view);
